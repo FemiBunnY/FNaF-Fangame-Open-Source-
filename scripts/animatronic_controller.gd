5cef2260b26ff6e03c74d967cee9a09d18e4536b
FemiBunnY <femibunny@gmail.com>
2024-07-13 02:53:36 -0400

    animatronic path movement update

--- scripts/animatronic_controller.gd
+++ scripts/animatronic_controller.gd
@@ -6,6 +6,7 @@ signal enter_move
 
 var phase:int
 var player_on_ventilation_l:bool = false
+var lights_on:bool = true
 
 func _ready():
 	timer.start()
@@ -17,19 +18,29 @@ func _on_next_movement_timeout():
 		reparent($"../stage to window/stw follow")
 		emit_signal("enter_move")
 	elif phase == 2:
-		print("esperando para atacar o regresar")
-		timer.start()
-		phase = 3
+		pass
 	elif phase == 3:
-		if player_on_ventilation_l:
+		if player_on_ventilation_l and lights_on:
 			print("regret to stage")
+			reparent($"../window to stage/wts follow")
+			emit_signal("enter_move")
+			phase = 4
+		elif player_on_ventilation_l and not lights_on:
+			print("go to vent")
 		elif not player_on_ventilation_l:
 			print("atack")
 		
 func _on_stw_follow_path_has_end():
+	print("path has end")
 	if phase == 1:
 		phase = 2
 		reparent($"../../../../window")
+		print("esperando para atacar o regresar")
+		timer.start()
+		phase = 3
+	elif phase == 4:
+		reparent($"../../../../stage")
+		phase = 1
 		timer.start()
 
 func _on_ventilation_l_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
@@ -39,3 +50,15 @@ func _on_ventilation_l_body_shape_entered(body_rid, body, body_shape_index, loca
 func _on_ventilation_l_body_shape_exited(body_rid, body, body_shape_index, local_shape_index):
 	if body.name == "Player":
 		player_on_ventilation_l = false
+
+func _on_office_lights_off():
+	lights_on = false
+
+func _on_office_lights_on():
+	lights_on = true
+
+func _on_wtv_follow_path_has_end():
+	pass
+
+func _on_wts_follow_path_has_end():
+	pass
--- scripts/office_controll.gd
+++ scripts/office_controll.gd
@@ -1,5 +1,8 @@
 extends Node3D
 
+signal lights_on
+signal lights_off
+
 @onready var light_1:OmniLight3D = $Lamps_02_001/OmniLight3D
 @onready var light_2:OmniLight3D = $Lamps_02_002/OmniLight3D
 
@@ -10,7 +13,9 @@ func _on_player_interaction():
 		light_1.light_energy = 1
 		light_2.light_energy = 1
 		on = false
+		emit_signal("lights_on")
 	elif not on:
 		light_1.light_energy = 0
 		light_2.light_energy = 0
 		on = true
+		emit_signal("lights_off")
